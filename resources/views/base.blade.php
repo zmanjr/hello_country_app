af2f5a9b99680c03e686eac34509f9b72aad514a
Arantez Mendez <zmanjrgit@gmail.com>
2022-04-30 21:48:42 +0200

    Add proper title to pages

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Hello City</title>
+        <title>@yield('title','Hello City')</title>
     </head>
     <body class="antialiased">
         @yield('content')
--- resources/views/pages/about_us.blade.php
+++ resources/views/pages/about_us.blade.php
@@ -1,5 +1,6 @@
 @extends('base')
 
+@section('title','About us | Hello City')
 @section('content')
        <h1>Built with &hearts; by Zman Jr</h1>
        <p>It's currently {{ date('h:i A')}}. <a href = "/">Back to welcome page</a></p>
