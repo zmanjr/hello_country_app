44d4a327d2ac3048c809e9a614040df5c3c9cbb8
Arantez Mendez <zmanjrgit@gmail.com>
2022-04-30 21:40:01 +0200

    Remove duplication by using app layout

--- resources/views/pages/about_us.blade.php
+++ resources/views/pages/about_us.blade.php
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>About Us | Hello City</title>
-    </head>
-    <body class="antialiased">
+@extends('base')
+
+@section('content')
        <h1>Built with &hearts; by Zman Jr</h1>
        <p>It's currently {{ date('h:i A')}}. <a href = "/">Back to welcome page</a></p>
 
-       <footer>
-           <p>&copy; Copyright {{ date('Y')}} &middot; </p>
-       </footer>
-    </body>
-</html>
+@endsection
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Hello City</title>
-    </head>
-    <body class="antialiased">
-       <h1>Hello from Benin</h1>
-       <p>It's currently {{ date('h:i A')}}</p>
+@extends('base')
 
-       <footer>
-           <p>&copy; Copyright {{ date('Y')}} &middot; <a href="/about-us">About Us</a></p>
-       </footer>
-    </body>
-</html>
+@section('content')
+<h1>Hello from Benin</h1>
+<p>It's currently {{ date('h:i A')}}</p>
+@endsection
