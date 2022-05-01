4d642ebe495634098e9ecc97caf3f92cdb222676
Arantez Mendez <zmanjrgit@gmail.com>
2022-05-01 21:16:17 +0200

    Add images

--- resources/views/pages/about_us.blade.php
+++ resources/views/pages/about_us.blade.php
@@ -2,6 +2,7 @@
 
 @section('title','About us | '. config('app.name'))
 @section('content')
+       <img src="/images/benin.jpg" alt="benin" style="border-radius : 50%;"/>
        <h1>Built with &hearts; by Zman Jr</h1>
        <p>It's currently {{ date('h:i A')}}. <a href = "{{route('app_home')}}">Back to welcome page</a></p>
 
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,6 +1,7 @@
 @extends('base')
 
 @section('content')
+<img src="/images/benin.jpg" alt="benin"/>
 <h1>Hello from Benin</h1>
 <p>It's currently {{ date('h:i A')}}</p>
 @endsection
