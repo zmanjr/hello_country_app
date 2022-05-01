982591add38c4d942080f3a2fe83d600cf36f802
Arantez Mendez <zmanjrgit@gmail.com>
2022-05-01 14:07:56 +0200

    Remove using of end and use config instead

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>@yield('title',env('APP_NAME'))</title>
+        <title>@yield('title',config('app.name'))</title>
     </head>
     <body class="antialiased">
         @yield('content')
--- resources/views/pages/about_us.blade.php
+++ resources/views/pages/about_us.blade.php
@@ -1,6 +1,6 @@
 @extends('base')
 
-@section('title','About us | '. env('APP_NAME'))
+@section('title','About us | '. config('app.name'))
 @section('content')
        <h1>Built with &hearts; by Zman Jr</h1>
        <p>It's currently {{ date('h:i A')}}. <a href = "/">Back to welcome page</a></p>
