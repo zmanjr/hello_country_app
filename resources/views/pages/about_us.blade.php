95288c05c9b91f92df4ed6690b63a6166648faf1
Arantez Mendez <zmanjrgit@gmail.com>
2022-05-01 21:32:27 +0200

    Use asset helper function to load image

--- resources/views/pages/about_us.blade.php
+++ resources/views/pages/about_us.blade.php
@@ -2,7 +2,7 @@
 
 @section('title','About us | '. config('app.name'))
 @section('content')
-       <img src="/images/benin.jpg" alt="benin" style="border-radius : 50%;"/>
+       <img src="{{asset('/images/benin.jpg')}}" alt="benin" style="border-radius : 50%;height : 100px; width : 100px;"/>
        <h1>Built with &hearts; by Zman Jr</h1>
        <p>It's currently {{ date('h:i A')}}. <a href = "{{route('app_home')}}">Back to welcome page</a></p>
 
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('base')
 
 @section('content')
-<img src="/images/benin.jpg" alt="benin"/>
+<img src="{{asset('/images/benin.jpg')}}" alt="benin" style="height : 100px; width : 100px;"/>
 <h1>Hello from Benin</h1>
 <p>It's currently {{ date('h:i A')}}</p>
 @endsection
