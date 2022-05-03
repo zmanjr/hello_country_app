6b436c381338fa2e95ee704dd77bd4f76a7c781b
Arantez Mendez <zmanjrgit@gmail.com>
2022-05-03 15:42:12 +0200

    Update helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -1,7 +1,7 @@
 <?php
 
 if(!function_exists('page_title')){
-    function page_title(?string $title = null){
+    function page_title(?string $title = null):string{
         return $title ? $title . ' | '. config('app.name') : config('app.name');
     }
 }
